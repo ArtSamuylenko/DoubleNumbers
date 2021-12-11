721110089cdcb590d32ca5adcad8cf3e409e8146
ArtSamuylenko <89385274775@mail.ru>
2021-12-12 02:09:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace double
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int a = int.Parse(Console.ReadLine());
+            int result = 0;
+            int count = 0;
+            for (int i = 2; i <= a; i += 2)
+            {
+                result += i;
+                count += 1;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
